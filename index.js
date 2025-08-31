9d8a6059ec5b27f2ae9e9ff3dc287ffa453448bb
Osiris <ricardomartin9922@gmail.com>
2025-08-31 10:59:42 +0200

    Upload new version with dynamic file

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const fs = require('fs');
 
-const requiredFiles = [
+// Leemos el fichero con los nombres de los archivos
+const filesListPath = 'filesExistance.txt';
+
+if (!fs.existsSync(filesListPath)) {
+  console.error(`❌ No se encuentra el fichero ${filesListPath}`);
+  process.exit(1);
+}
+
+// Cargamos y limpiamos la lista
+//Works in sigle files and folders
+const requiredFiles = fs.readFileSync(filesListPath, 'utf8')
+  .split('\n')                // dividimos por líneas
+  .map(line => line.trim())   // quitamos espacios
+  .filter(line => line !== '' && !line.startsWith('#')); // ignoramos vacías o comentarios
+
+//If yo want use array instead of dynamic file
+/*const requiredFiles = [
   'README.md',
   'index.js',
   'package.json',
   'prueba.txt',
   'src/prueba2.txt'
-];
+];*/
 
 let missing = [];
 
